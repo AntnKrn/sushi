4813e12722af4b41095b086d45c886c067bff9e2
Anton Kuryan <super.tosh2016@yandex.by>
2024-03-20 14:06:48 +0300

    fix carousel

--- frontend/src/widgets/news/ui/index.tsx
+++ frontend/src/widgets/news/ui/index.tsx
@@ -1,38 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import "./index.scss"; 
+import "./index.scss";
 
 import img1 from "./img1.jpg";
 import img2 from "./img2.jpg";
 import img3 from "./img3.jpg";
 import img4 from "./img4.jpg";
-import img5 from "./img5.jpg";
-
+import img5 from "./img6.jpg";
 
 export const News = () => {
-    const imagesUrls: string[] = [img1, img2, img3, img4, img5];
+  const imagesUrls: string[] = [img1, img2, img3, img4, img5];
 
-    const [imageIndex, setImageIndex] = useState(0);
+  const [imageIndex, setImageIndex] = useState(0);
 
-    const showNextImage = () => {
-        setImageIndex(index => {
-            if(index === imagesUrls.length - 1) return 0;
-            return index + 1;
-        })
-    }
+  useEffect(() => {
+    const toggle = setInterval(() => {
+      setImageIndex((index) => {
+        if (index === imagesUrls.length - 1) return 0;
+        return index + 1;
+      });
+    }, 4000);
 
-    const showPrevImage = () => {
-        setImageIndex(index => {
-            if(index === 0) return imagesUrls.length -1;
-            return index - 1;
-        })
-    }
+    return () => clearInterval(toggle);
+  });
 
-    return (
-        <div className="news-wrapper" style={{ width: '100%', height: "100%", margin: "0 auto", position: 'relative'}}>
-                <img src={imagesUrls[imageIndex]} className="img-slider" />
-                <button onClick={showPrevImage} className="img-slider-btn" style={{ left: 0 }}>Prev</button>
-                <button onClick={showNextImage} className="img-slider-btn" style={{ right: 0 }}>Next</button>
-        </div>
-    );
-}
+  const showNextImage = () => {
+    setImageIndex((index) => {
+      if (index === imagesUrls.length - 1) return 0;
+      return index + 1;
+    });
+  };
+
+  const showPrevImage = () => {
+    setImageIndex((index) => {
+      if (index === 0) return imagesUrls.length - 1;
+      return index - 1;
+    });
+  };
+
+  return (
+    <div style={{ position: "relative" }}>
+      <div className="news-wrapper">
+        <img src={imagesUrls[imageIndex]} className="img-slider" />
+        <button
+          onClick={showPrevImage}
+          className="img-slider-btn"
+          style={{ left: 0 }}
+        >
+          Prev
+        </button>
+        <button
+          onClick={showNextImage}
+          className="img-slider-btn"
+          style={{ right: 0 }}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+};
